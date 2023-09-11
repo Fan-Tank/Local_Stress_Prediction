2deb85f3a15694f268aa75e017e62de5b3333008
Fan-Tank <76890876+Fan-Tank@users.noreply.github.com>
2023-09-11 14:16:52 +0800

    GenerateDataset_plugin.py

--- Local_Stress_Prediction/GenerateDataset/GenerateDataset_plugin.py
+++ Local_Stress_Prediction/GenerateDataset/GenerateDataset_plugin.py
@@ -1,19 +1,25 @@
+# Import necessary modules
 from abaqusGui import getAFXApp, Activator, AFXMode
 from abaqusConstants import ALL
 import os
+
+# Get the absolute path and directory of the current script file
 thisPath = os.path.abspath(__file__)
 thisDir = os.path.dirname(thisPath)
 
+# Get the plugin toolset of the current ABAQUS program
 toolset = getAFXApp().getAFXMainWindow().getPluginToolset()
+
+# Register a GUI menu button
 toolset.registerGuiMenuButton(
-    buttonText='GenerateDataset',
-    object=Activator(os.path.join(thisDir, 'GenerateDatasetDB.py')),
-    kernelInitString='import GenerateDataset',
-    messageId=AFXMode.ID_ACTIVATE,
-    icon=None,
-    applicableModules=ALL,
-    version='N/A',
-    author='N/A',
-    description='N/A',
-    helpUrl='N/A'
+    buttonText='GenerateDataset',  # Text displayed on the button
+    object=Activator(os.path.join(thisDir, 'GenerateDatasetDB.py')),  # Activator object for the plugin
+    kernelInitString='import GenerateDataset',  # Initialization string for the plugin
+    messageId=AFXMode.ID_ACTIVATE,  # Message ID
+    icon=None,  # Icon (set to None here)
+    applicableModules=ALL,  # Applicable modules (all modules)
+    version='N/A',  # Version information
+    author='N/A',  # Author information
+    description='N/A',  # Description
+    helpUrl='N/A'  # Help URL
 )
